691bc383bea64e5a0545d2fa453edc0bd8890eef
Levi Nicklas <levinicklas@levis-mbp.home>
2020-04-17 21:36:53 -0400

    Update Main README.md

--- Covid-Opec/Scripts/GetNewsData.R
+++ Covid-Opec/Scripts/GetNewsData.R
@@ -6,3 +6,9 @@
 library(twitteR)
 library(tidyverse)
 
+source("news.R")
+
+
+keywords <- c("opec", "OPEC","oil", "Coronavirus", "coronavirus", "COVID")
+# May want to leave term empty to just search top headlines.
+# OR search other news, like USA 
